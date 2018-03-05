c9310d947468fee2f94edaac6644fad4252d3d07
Geoffrey Girard <geoffreygirard28@gmail.com>
2018-03-05 21:19:44 +0100

    update

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 var home = document.getElementById('game-start');
 var game = document.getElementById('game');
+var gameOver = document.getElementById('game-over');
 var buttonHome = document.querySelector('#game-start button');
+var buttonGameOver = document.querySelector('#game-over button');
 var answers = document.querySelectorAll('.flag img');
 var answerName = document.getElementById('countriesName');
+var answer = {};
+var lives = document.querySelectorAll('.lives img');
+var counterLives = 3;
+var score = document.querySelector('.score strong');
+var counterScore = 0;
+var timer = document.querySelector('.time span');
 
 buttonHome.addEventListener('click', function() {
   home.classList.toggle('is-open');
   game.classList.toggle('is-open');
+  counterLives = 3;
   displayFlag();
   runGame();
 });
 
+buttonGameOver.addEventListener('click', function(){
+  gameOver.classList.toggle('is-open');
+  home.classList.toggle('is-open');
+  for (var i = 0; i < lives.length; i++) {
+    lives[i].classList.toggle('is-active');
+  }
+});
+
 function displayFlag() {
-    var answer = flags[Math.floor(Math.random()*102)]; // Choisi un pays au hasard dans la liste
+    answer = flags[Math.floor(Math.random()*102)]; // Choisi un pays au hasard dans la liste
     answerName.textContent = answer.name;  // Fais apparaître le nom du pays à trouver
 
     // Boucle qui ajoute les 4 drapeaux (choix de réponses)
     for (var i = 0; i < answers.length; i++) {
-      var value = flags[Math.floor(Math.random()*flags.length)];
+      var value = flags[Math.floor(Math.random()*flags.length+1)];
       answers[i].src = "flags/" + value.code + ".svg";
-      console.log(answers[i]);
     }
     // Ajoute le pays à trouver parmi les 4 propositions
     var randomAnswer = [Math.floor(Math.random()*4)];
@@ -31,13 +47,30 @@ function runGame() {
       answers[i].addEventListener('click', function() {
         var verifyAnswer = this.src[49] + this.src[50];
         if (verifyAnswer === answer.code) {
-          console.log('win');
+          displayFlag();
+          addScore();
         } else {
-          console.log('lose');
+          removeLives();
         }
       });
     }
 }
+
+function addScore() {
+  counterScore++;
+  score.textContent = counterScore;
+}
+
+function removeLives() {
+  counterLives--;
+  lives[counterLives].classList.toggle('is-active');
+  if (counterLives == 0) {
+    game.classList.toggle('is-open');
+    gameOver.classList.toggle('is-open');
+  }
+}
+
+
 
 
 
