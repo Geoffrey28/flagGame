665740eecdd3ba78d5c49a9cc003abbd7f69087c
Geoffrey28 <geoffreygirard28@gmail.com>
2018-03-08 16:52:18 +0100

    update

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ var buttonHome = document.querySelector('#game-start button');
 var buttonGameOver = document.querySelector('#game-over button');
 var answers = document.querySelectorAll('.flag img');
 var answerName = document.getElementById('countriesName');
-//var answer = {};
+var answer = {};
 var lives = document.querySelectorAll('.lives img');
-var counterLives = 3;
 var score = document.querySelector('.score strong');
-var counterScore = 0;
 var timer = document.querySelector('.time span');
 var counterTimer = 20;
 
@@ -39,19 +37,33 @@ function displayFlagFinder() {
 
     answer = flags[Math.floor(Math.random()*102)]; // Choisi un pays au hasard dans la liste
     answerName.textContent = answer.name;  // Fais apparaître le nom du pays à trouver
+
+
     // Boucle qui ajoute les 4 drapeaux (choix de réponses)
     for (var i = 0; i < answers.length; i++) {
       var value = flags[Math.floor(Math.random()*flags.length+1)];
       do {
         var value = flags[Math.floor(Math.random()*flags.length+1)];
-      } while (value.code === answer.code);
+      } while (value.name === answer.name);
       answers[i].src = "flags/" + value.code + ".svg";
     }
+    
     // Ajoute le pays à trouver parmi les 4 propositions
     var randomAnswer = [Math.floor(Math.random()*4)];
     answers[randomAnswer].src = "flags/" + answer.code + ".svg";
-
 }
+
+
+
+    // TRI COULEURS
+/*    for (var i = 0; i < answer.colors; i++) {
+      for (var a = 0; a < value.colors; a++) {
+        do {
+          var value = flags[Math.floor(Math.random()*flags.length+1)];
+        } while (answer[i].colors != value[i].colors || value.code == answer.code);
+      }
+    }
+    answers[i].src = "flags/" + value.code + ".svg"; */
 
 function runGame() {
     for (let i = 0; i < answers.length; i++) {
@@ -63,7 +75,7 @@ function runGame() {
           if (counterTimer < 30) {
             counterTimer = counterTimer + 4;
           }
-        } else {
+        } else if (answers[i].className != 'is-active') {
           answers[i].classList.add('is-active');
           removeLives();
         }
@@ -90,13 +102,12 @@ function clock()  {
         timer.textContent = counterTimer;
         counterTimer--;
      } else if (counterTimer == 0) {
-        //clearInterval(intervalId)
         flagFinder.classList.remove('is-open');
         gameOver.classList.add('is-open');
         counterTimer--;
      }
 };
 
-function setClock(){
+function setClock() {
      intervalId = setInterval(clock, 1000);
 };
